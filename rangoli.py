4cc0d9174f35cca5ccdde3bb84e168d1b0e556ca
shash05 <67625805+shash05@users.noreply.github.com>
2020-07-07 21:27:42 +0530

    Update rangoli.py
    i came up with the short cut of the code  ,hope it looks good

--- rangoli.py
+++ rangoli.py
@@ -47,42 +47,19 @@ j-i-h-g-f-e-d-c-b-a-b-c-d-e-f-g-h-i-j
 The center of the rangoli has the first alphabet letter a, and the boundary has the Nth alphabet letter (in alphabetical order).
 """
 
-import string
-from collections import defaultdict
-d = defaultdict(list)
-num = input()
-alph = list(string.ascii_lowercase)
-# print alph
+def print_rangoli(n):
+    l1=list(map(chr,range(97,123)))
+    x=l1[(n-1)::-1]+l1[1:n]
+    m=len('-'.join(x))
+    #print(m)
 
+    for i in range(1,n):
+        print('-'.join(l1[n-1:n-i:-1]+l1[n-i:n]).center(m,'-'))
 
-# for i in xrange(num):
-# 	print ((alph[num])+'-'*i).rjust(num*2,'-')+'#-'+('#-'*i).ljust(num*2,'-')
+    for i in range(n,0,-1):
+        print('-'.join(l1[n-1:n-i:-1]+l1[n-i:n]).center(m,'-'))
 
-# for i in xrange(1,num):
-# 	print (('#-'*(num-i-1)).rjust(num*2,'-')+'#-'+('#-'*(num-i-1)).ljust(num*2,'-'))
-
-for i in range(1,(2*num-1)+1):
-	for j in range(1,(2*(2*num-1)-1)+1):
-		d[i].append('-')
-
-k = (len(d)*2)//2
-temp = (len(d)//2 )
-for i in range(1,len(d)+1):
-	if i <= (len(d)+1)//2:
-		index = num
-		middle  = (len(d[i])+1)//2
-		for j in range(k,k+4*i-3,2):
-			d[i][j-1] = alph[index-1]
-			if j >= middle:
-				index = index +1
-			else:
-				index = index -1
-		k = k-2
-	else:
-		d[i] = d[temp]
-		temp = temp -1
-		
-# print d
-
-for i in range(1,len(d)+1):
-	print ''.join(d[i])
+ 
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
